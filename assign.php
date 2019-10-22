d4f4a7d23ef9c9c16a1af6dd873d8097e82e0938
fatimahyderi <fatimahyderi84@gmail.com>
2019-10-22 11:12:28 +0500

    assignment

---
+++ assign.php
@@ -0,0 +1,28 @@
+<?php
+
+function countchars($x,$y,$z){
+if(strlen($x)<$y){
+	$newstring = str_pad($x,$y,$x);
+	echo $newstring;
+	echo '</br>';
+	$return = substr_count( $newstring , $z );
+	
+}
+else{
+	$str = substr($x,0,$y);
+	echo $str;
+	echo '</br>';
+	$return = substr_count( $str, $z);
+	
+}
+return $return;
+}
+
+	$string = "abcac";
+	$n = 10;
+	$find = 'a';
+
+$ans = countchars($string,$n,$find);
+ echo 'There are ' . $ans . ' occurrences of ' . $find ;
+  
+?>
